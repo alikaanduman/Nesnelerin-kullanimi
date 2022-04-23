791c30a1e17a0f9914de63e9b3ca6396ffa75250
Ali Kaan Duman <alikaanduman@hotmail.com>
2022-04-23 15:44:54 +0300

    C# Nesnelerin Kullanımı

---
+++ Nesnelerin Kullanımı/Nesnelerin Kullanımı/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nesnelerin_Kullanımı
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            comboBox1.Text = "";
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Form2 f2 = new Form2();
+            f2.Show();
+
+            f2.label1.Text = "Sonuçlarınız aşağıda listelenmiştir ---> ";
+            
+            if (radioButton1.Checked==true)
+            {
+                f2.label11.Text = "Bekarsın";
+
+            }
+            else if (radioButton2.Checked==true)
+            {
+                f2.label11.Text = "Evlisin";
+            }
+            else if (radioButton3.Checked==true)
+            {
+                f2.label11.Text = "Boşanmışsın";
+            }
+
+            if (checkBox1.Checked==true)
+            {
+                f2.label10.Text = "Film İzlemeyi seviyorsun".ToString();
+            }
+            else if (checkBox2.Checked == true)
+            {
+               f2.label10.Text = "Kitap okumayı seviyorsun".ToString();
+            }
+            else if (checkBox3.Checked == true)
+            {
+                f2.label10.Text = "Tatil yapmayı seviyorsun".ToString();
+            }
+
+            if (checkBox1.Checked==true&&checkBox2.Checked==true)
+            {
+                f2.label10.Text="Film İzlemeyi ve kitap okumayı seviyorsun".ToString();
+            }
+
+            if (checkBox2.Checked == true && checkBox3.Checked == true)
+            {
+                f2.label10.Text = "Kitap Okumayı ve Tatil Yapmayı seviyorsun".ToString();
+            }
+            if (checkBox1.Checked == true && checkBox3.Checked == true)
+            {
+                f2.label10.Text = "Film İzlemeyi ve Tatil yapmayı seviyorsun".ToString();
+            }
+
+            if (checkBox1.Checked == true && checkBox2.Checked == true && checkBox3.Checked==true)
+            {
+                f2.label10.Text = "Film İzlemeyi, kitap okumayı ve Tatil yapmayı seviyorsun".ToString();
+            }
+
+            f2.listBox1.Items.Add(textBox1.Text);
+            f2.listBox1.Items.Add(textBox2.Text);
+            f2.listBox1.Items.Add(textBox3.Text);
+            f2.listBox1.Items.Add(comboBox1.Text);
+            f2.listBox1.Items.Add(dateTimePicker1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "NESNELERİN KULLANIMI";
+            comboBox1.Items.Add("Bursa");
+            comboBox1.Items.Add("Ankara");
+            comboBox1.Items.Add("İstanbul");
+            comboBox1.Items.Add("Burdur");
+            comboBox1.Items.Add("Adana");
+            comboBox1.Items.Add("Düzce");
+            comboBox1.Items.Add("Erzurum");
+            comboBox1.Items.Add("Kastamonu");
+
+        }
+    }
+}
---
+++ Nesnelerin Kullanımı/Nesnelerin Kullanımı/Form2.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Nesnelerin_Kullanımı
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(91, 309);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "label2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(166, 119);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(35, 13);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "label2";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 164);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(343, 121);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(393, 349);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label10;
+        public System.Windows.Forms.Label label11;
+        public System.Windows.Forms.ListBox listBox1;
+    }
+}
